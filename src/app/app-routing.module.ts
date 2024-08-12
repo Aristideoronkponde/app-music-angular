514ef7e18acf33c3f0cd7157dda11a72d42f25b8
Aristideoronkponde <legroscapo@gmail.com>
2024-08-12 10:32:49 +0100

    mise en place de la navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './album/album.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/album', pathMatch: 'full' },
+  {path:'album',component:AlbumComponent},
+  {path:'login',component:LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AlbumComponent } from './album/album.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AlbumComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
